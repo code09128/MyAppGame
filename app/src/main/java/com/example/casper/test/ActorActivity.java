b4c429cab49eba43925fb9b79ae0af39e3541e1c
DustinHsieh <code09128@gmail.com>
2017-10-13 11:19:49 +0800

    main atk text

--- app/src/main/java/com/example/casper/test/ActorActivity.java
+++ app/src/main/java/com/example/casper/test/ActorActivity.java
@@ -21,14 +21,14 @@ public class ActorActivity extends AppCompatActivity {
         new CountDownTimer(5000, 1000) {
 
             public void onTick(long millisUntilFinished) {
+
                 tv1.setText("準備開始戰鬥: " + millisUntilFinished / 1000);
+
             }
 
             public void onFinish() {
 
-                for (int i=0; i<50; i++)
-                    tv1.setText(tv1.getText()+"\n"+Integer.toString(i));
-                tv1.setText("done!");
+                tv1.setText("開始戰鬥!");
             }
 
         }.start();
